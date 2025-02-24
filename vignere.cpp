d955f20fcda27a9ab1604fb84f6b6a4dff1b02db
MohammedZaheed <zaheedmohammed32@gmail.com>
2025-02-24 22:34:54 +0530

    Add files via upload

---
+++ vignere.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    char val[26][26];
+    for(int i=0;i<26;i++){
+        for(int j=0;j<26;j++){
+            val[i][j]=(i+j)%26+'a';
+        }
+    }
+    string pt="ise";
+    string key="roc";
+    string ck="";
+    for(int i=0;i<pt.size();i++){
+        ck += val[pt[i]-'a'][key[i%key.size()]-'a'];
+    }
+    cout << ck ;
+}
